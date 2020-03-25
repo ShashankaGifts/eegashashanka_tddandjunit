d5c4c1be5ad43c17c4826bb0b1de6e39452ba42a
ShashankaGifts <60562385+ShashankaGifts@users.noreply.github.com>
2020-03-25 18:07:13 +0530

    Update RemoveFirst2A.java

--- src/main/java/junit_tdd/RemoveFirst2A.java
+++ src/main/java/junit_tdd/RemoveFirst2A.java
@@ -1,5 +1,23 @@
 package junit_tdd;
 
 public class RemoveFirst2A {
+	public String remove(String string) {
+		String res = string;
+		if(string.length()<=0)
+			res = "";
+		else{
+			if(string.charAt(0) == 'A' && string.charAt(1) == 'A'){
+				res= string.substring(2,string.length());
+			}
+			else if(string.charAt(0) == 'A' || string.charAt(1) == 'A'){
+				if(string.charAt(0) == 'A' )
+					res = string.substring(1,string.length());
+				else
+					res = string.charAt(0)+string.substring(2, string.length());
+			}
+		}
+		return res;
+	}
+
 
 }
